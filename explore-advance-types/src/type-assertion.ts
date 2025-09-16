c4dd8f96d9901805769a329c29e1852be2869007
Md Minhaz Uddin <md.minhazuddin2597@gmail.com>
2025-09-16 22:11:32 +0600

    type-assertion is added

---
+++ explore-advance-types/src/type-assertion.ts
@@ -0,0 +1,17 @@
+{
+  //   type assertion
+
+  const kgToGm = (value: string | number): string | number | undefined => {
+    if (typeof value === "string") {
+      const convertedValue = parseFloat(value) * 1000;
+      return `The converted value is : ${convertedValue}`;
+    }
+    if (typeof value === "number") {
+      return value * 1000;
+    }
+  };
+  const result1 = kgToGm("1000") as string;
+  const result2 = kgToGm(1000) as number;
+
+  console.log(result1, result2);
+}
